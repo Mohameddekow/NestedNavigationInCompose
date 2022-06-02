a114d93ef64256b70855d367de20c82229108f7f
mohamed <mohameddekow77@gmail.com>
2022-06-02 20:40:56 +0300

    passing args between composable

--- app/src/main/java/com/example/nestednavigationincompose/Screen.kt
+++ app/src/main/java/com/example/nestednavigationincompose/Screen.kt
@@ -1,6 +1,14 @@
 package com.example.nestednavigationincompose
 
+const val DETAIL_SCREEN_ID = "id"
 sealed class Screen(val route: String) {
+
     object HomeScreen : Screen(route = "home_screen")
-    object DetailScreen : Screen(route = "details_screen")
+    object DetailScreen : Screen(route = "details_screen/{$DETAIL_SCREEN_ID}") {
+        fun passId(id: Int): String {
+//            return this.route.replace(oldValue ="{$DETAIL_SCREEN_ID}" , newValue = id.toString())
+
+            return "detail_screen/$id"
+        }
+    }
 }
